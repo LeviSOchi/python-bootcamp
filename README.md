dc64e7e17728df47277f2f2073649161a3db3f6c
Levi Ochi <leviochi12@gmail.com>
2025-04-17 16:56:13 -0300

    Adicionando o readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# bootcamp_python
+## Calendário Bootcamp - Python do zero
+
+Um intensivo único com tópicos avançados para resolver problemas reais
+
+| Aula  | Workshop                                                                 |
+|-------|--------------------------------------------------------------------------|
+| Aula 01 | Python, Git e VScode: Python do Zero                                    |
+| Aula 02 | TypeError, Type Check, Type Conversion, try-except e if                |
+| Aula 03 | Controle de Fluxo: DEBUG, IF, FOR, While, Listas e Dicionários         |
+| Aula 04 | Tipos complexos e Type Hint (Dicionários vs DataFrames Vs Tabelas Vs Excel) |
+| Aula 05 | Projeto 01: Leitura e Escrita de Arquivos, lendo 1 bilhão de linhas    |
+| Aula 06 | Exercício de revisão                                                    |
+| Aula 07 | Funções em Python e Estrutura de Dados - Parte 1                       |
+| Aula 08 | Funções em Python e Estrutura de Dados - Parte 2                       |
+| Aula 09 | Funções em Python e Estrutura de Dados - Parte 3                       |
+| Aula 10 | Aula de revisão                                                         |
+| Aula 11 | Introdução a POO                                                        |
+| Aula 12 | Introdução às Classes em Python - Parte 01                             |
+| Aula 13 | Introdução às Classes em Python - Parte 02                             |
+| Aula 14 | Introdução às Classes em Python - Parte 03                             |
+| Aula 15 | Introdução às Classes em Python - Parte 04                             |
+| Aula 16 | Aula de revisão de programação orientada a objetos + SQLModel          |
+| Aula 17 | SQLAlchemy - Conjunto de ferramentas para manipular SQL em Python      |
+| Aula 18 | O que é uma API? Request, Pydantic e fazendo nosso CRUD                |
+| Aula 19 | O que é uma API? Criando nossa primeira API                            |
+| Aula 20 | Nosso Projeto de CRUD Backend + Frontend + Banco de Dados              |
+       
---
+++ aula02_bootcamp/exercicios.py
@@ -0,0 +1,105 @@
+import math
+
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+print("Vamos somar dois números que você digitar")
+soma1 = int(input("Digite um número: "))
+soma2 = int(input("Digite outro número: "))
+soma = soma1+soma2
+print(soma)
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+print("Vamos calcular o resto da divisao do número que você digitar dividido por 5")
+resto_divisao1 = int(input("Digite um número: "))
+resto_divisao = resto_divisao1%5
+print(resto_divisao)
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+print("Vamos multiplicar dois números que você digitar")
+multi1 = int(input("Digite um número: "))
+multi2 = int(input("Digite outro número: "))
+multiplicacao = multi1*multi2
+print(multiplicacao)
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+print("Vamos fazer a divisão inteira do primeiro número pelo segundo número que você digitar")
+divisao1 = int(input("Digite um número: "))
+divisao2 = int(input("Digite outro número: "))
+divisao_inteira = divisao1//divisao2
+print(divisao_inteira)
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+print("Vamos fazer a potência do número que você digitar")
+potencia1 = int(input("Digite um número: "))
+potencia = potencia1**2
+print(potencia)
+
+# #### Números de Ponto Flutuante (`float`)
+
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+print("Vamos calcular a área do raio do círculo que você digitar")
+raio_circulo = float(input("Digite o raio do círculo: "))
+area_circulo = math.pi  * raio_circulo ** 2
+print(f"{area_circulo:.2f}")
+
+# #### Strings (`str`)
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+
+# #### try-except e if
+
+# 21: Conversor de Temperatura
+
+
+# 22: Verificador de Palíndromo
+
+
+# 23: Calculadora Simples
+
+
+# 24: Classificador de Números
+
+
+# 25: Conversão de Tipo com Validação
