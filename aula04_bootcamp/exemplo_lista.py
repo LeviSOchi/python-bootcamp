fb7a4219576da9aa7b346f7aa65c9a44fda7a400
Levi Ochi <leviochi12@gmail.com>
2025-04-19 11:13:29 -0300

    Adicionando exemplo de lista

---
+++ aula04_bootcamp/exemplo_lista.py
@@ -0,0 +1,11 @@
+produto_1: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "bermuda"
+
+produtos: list = []
+
+produtos.append(produto_1)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+print(produtos)
