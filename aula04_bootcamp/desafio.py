fe8ace54025fcf31033bff7ed0812c45bad73efb
Levi Ochi <leviochi12@gmail.com>
2025-04-21 09:21:23 -0300

    Adicionando arquivo desafio com exemplos de dicionarios e listas

--- aula04_bootcamp/desafio.py
+++ aula04_bootcamp/desafio.py
@@ -1,3 +1,20 @@
-numeros: list = list(range(1, 11))
-for numero in numeros:
-    print(numero**2)
+import json
+
+produtos = {
+    "livro_01":{"nome":"sapato",
+    "preco": 39.99,
+    "quantidade":20,
+    "disponibilidade": True},
+
+    "livro_02":{"nome":"camiseta",
+    "preco": 10.99,
+    "quantidade":20,
+    "disponibilidade": True}
+}
+
+carrinho = []
+carrinho.append(produtos["livro_01"])
+carrinho.append(produtos["livro_02"])
+
+json_arquivo = json.dumps(carrinho)
+print(json_arquivo)
