25005624ed1e195ce1f84c467a9889a0ef691ef4
Levi Ochi <leviochi12@gmail.com>
2025-04-23 18:08:05 -0300

    Excluindo aula05 pois criei um repositório para ela

--- aula04_bootcamp/exercicios_listas_dicionarios.py
+++ aula04_bootcamp/exercicios_listas_dicionarios.py
@@ -1,3 +1,6 @@
+import typing
+from typing import Dict
+
 ## Exercícios de Listas e Dicionários
 
 #1 Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
@@ -6,14 +9,14 @@ for numero in numeros:
     print(numero**2)
 
 #2 Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
-linguagens = ["Python", "Java", "C++", "JavaScript"]
+linguagens: list = ["Python", "Java", "C++", "JavaScript"]
 linguagens.remove("C++")
 linguagens.append("Ruby")
 
 print(linguagens)
 
 #3 Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
-livro = {
+livro: Dict = {
     "Título":"Harry Potter",
     "Autor":"J. K. Rowling",
     "ano": 2000
@@ -24,7 +27,7 @@ for chave,valor in livro.items():
 
 #4 Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 def contar_caracteres(s):
-    contagem = {}
+    contagem: Dict = {}
     for caractere in s:
         contagem[caractere] = contagem.get(caractere, 0) + 1
     return contagem
@@ -32,23 +35,23 @@ def contar_caracteres(s):
 print(contar_caracteres("engenharia de dados"))
 
 #5 Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
-lista_compras = ["maçã", "banana", "cereja"]
-precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
-total = sum(precos[item] for item in lista_compras)
+lista_compras: list = ["maçã", "banana", "cereja"]
+precos: Dict = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+total: float = sum(precos[item] for item in lista_compras)
 print(f"Preço total: {total}")
 
 ## Exercícios intermediários à avançado
 
 #6 Objetivo: Dada uma lista de emails, remover todos os duplicados.
-emails = ["user@example.com", "admin@example.com", "user@example.com", "manager@example.com"]
-emails_unicos = list(set(emails))
+emails: list = ["user@example.com", "admin@example.com", "user@example.com", "manager@example.com"]
+emails_unicos: str = list(set(emails))
 
 print(emails_unicos)
 
 #7 Objetivo: Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
-idades = [12,14,23,45,23,57,12,23,12,11,69,24]
+idades: list= [12,14,23,45,23,57,12,23,12,11,69,24]
 
-idades_validas = [idade for idade in idades if idade >= 18]
+idades_validas: list = [idade for idade in idades if idade >= 18]
 print("Idades válidas ")
 
 #8 Objetivo: Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
