9cc2d8a7f0a75dee84a473bebd4b2b1e11ce7194
Levi Ochi <leviochi12@gmail.com>
2025-05-02 20:49:47 -0300

    Adicionando exemplo de função

---
+++ aula07_bootcamp/exemplo_00.py
@@ -0,0 +1,15 @@
+def minha_funcao():
+    return "Hello, World!"
+
+
+# funcao = agilidade na sustentação do código
+def soma(valor_1_para_somar: float, valor_2_para_somar: float) -> float:
+    """
+    Uma função simples de soma de valores do tipo float
+    """
+    resultado_da_soma = valor_1_para_somar + valor_2_para_somar
+    return resultado_da_soma
+
+valor_3 = soma(3,2)
+
+print(valor_3)
