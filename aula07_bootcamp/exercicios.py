986fc596684ef9d0c6779a8e06130d75684d5eef
Levi Ochi <leviochi12@gmail.com>
2025-05-03 11:39:08 -0300

    Adicionando os exercícios solucionados

--- aula07_bootcamp/exercicios.py
+++ aula07_bootcamp/exercicios.py
@@ -1,12 +1,36 @@
+from typing import List
+
 # revisão de funções
 
 # 1. Calcular Média de Valores em uma Lista
-from typing import List
-
 def calcular_media(valores: List[float]) -> float:
     return sum(valores) / len(valores)
 
-levi = calcular_media([10,10,10,10,10])
+#2. Filtrar dados acima de um limite
+def filtrardados(valores: List[float], limite: float) -> float:
+    resultado = []
+    for valor in valores:
+        if valor > limite:
+            resultado.append(valor)
+    return resultado
 
-print(levi)
+#3. Contar Valores Únicos em uma Lista
+def contar_valores_unicos(lista: List[int]) -> int:
+    return len(set(lista))
+
+#4. Converter Celsius para Fahrenheit em uma Lista
+def celsius_para_fahrenheit(temperaturas_celsius: List[float]) -> List[float]:
+    return [(9/5) * temp + 32 for temp in temperaturas_celsius]
+
+#5. Calcular Desvio Padrão de uma Lista
+def calcular_desvio_padrao(valores: List[float]) -> float:
+    media = sum(valores) / len(valores)
+    variancia = sum((x - media) ** 2 for x in valores) / len(valores)
+    return variancia ** 0.5
+
+#6. Encontrar Valores Ausentes em uma Sequência
+def encontrar_valores_ausentes(sequencia: List[int]) -> List[int]:
+    completo = set(range(min(sequencia), max(sequencia) + 1))
+    return list(completo - set(sequencia))
+
 
