23984a54e774e168b8af3bb8d1f4e98461ed0fc0
Levi Ochi <leviochi12@gmail.com>
2025-05-07 19:22:33 -0300

    Removendo arquivos para refazer aulas

--- aula09_bootcamp/src/main.py
+++
@@ -1,9 +0,0 @@
-from loguru import logger
-
-def minha_funcao():
-    raise ValueError("Um erro aconteceu!")
-
-try:
-    minha_funcao()
-except Exception:
-    logger.exception("Uma exceção foi capturada")
