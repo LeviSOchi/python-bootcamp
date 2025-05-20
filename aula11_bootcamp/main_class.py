188f397d857b023a6b2f8b6f6f3862544bf15d7d
Levi Ochi <leviochi12@gmail.com>
2025-05-19 22:25:33 -0300

    updata main com class

---
+++ aula11_bootcamp/main_class.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+class CsvProcessador:
+    def __init__ (self, path_arquivo):
+        self.path_arquivo = path_arquivo
+        self.df = None
+
+    def carregar_csv(self):
+        self.df = pd.read_csv(self.path_arquivo)
+
+    def filtrar_por(self, coluna, atributo):
+       return self.df [self.df[coluna] == atributo]
+    
+arquivo_csv = "./exemplo.csv"
+filtro = "estado"
+limite = "SP"
+
+arquivo_CSV = CsvProcessador(arquivo_csv)
+arquivo_CSV.carregar_csv()
+print(arquivo_CSV.filtrar_por(filtro,limite))
